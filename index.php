a10a3c8016a02a965a90a757d2d57e8a8ee33870
piojagcodes <piojag@gmail.com>
2023-05-22 16:45:56 +0200

    Redirection to Login.php

--- index.php
+++ index.php
@@ -11,7 +11,7 @@
 <body>
   <br />Only dead ones witnessed the end of war - Plato<br />
 
-  <form>
+  <form action="Login.php" method="post">
     Login: <br />
     <input type="text" name="login" /> <br />
     Password: <br />
