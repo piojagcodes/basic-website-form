bca24d848d9161d119d294f5cd18a48569e8f3e1
piojagcodes <piojag@gmail.com>
2023-05-22 19:28:35 +0200

    Add login functionality with database connection

---
+++ connect.php
@@ -0,0 +1,8 @@
+<?php
+
+    $HOST = "localhost"; // Hostname or IP address of the database server
+    $db_user = "root"; // Username for the database connection
+    $db_password = ""; // Password for the database connection
+    $db_name = "settlers"; // Name of the database to connect to
+
+?>
--- login.php
+++ login.php
@@ -1,8 +1,16 @@
 <?php
-    $login = $_POST['login'];
-    $password = $_POST['password'];
+    require_once "connect.php"; // Include the "connect.php" file for establishing a database connection
 
-    echo $login."<br />";  // Output the value of the 'login' variable
-    echo $password; // Output the value of the 'password' variable
+    $connection = @new mysqli($host, $db_user, $db_password, $db_name); // Create a new instance of the mysqli class for database connection
 
+    if ($connection->connect_errno != 0) { // Check if there is a connection error
+        echo "Error: " . $connection->connect_errno; // Output the connection error code
+    } else {
+        $login = $_POST['login']; // Get the value of the 'login' input from the POST request
+        $password = $_POST['password']; // Get the value of the 'password' input from the POST request
+
+        echo "It works!"; // Output the message "It works!"
+
+        $connection->close(); // Close the database connection
+    }
 ?>
