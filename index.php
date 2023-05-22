5a98f9eb13b8842d8d1ff22e477f5ef65796d605
piojagcodes <piojag@gmail.com>
2023-05-22 14:25:55 +0200

    Basic form with PHP and maybe other things

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Settlers - Website Game</title>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <style>
+    /* Your CSS styles go here */
+  </style>
+</head>
+<body>
+  <br />Only dead ones witnessed the end of war - Plato<br />
+
+  <form>
+    Login: <br />
+    <input type="text" name="login" /> <br />
+    Password: <br />
+    <input type="text" name="password" /> <br />
+    <input type="submit" value="Sign up" />
+  </form>
+
+  <header>
+    <!-- Your header content goes here -->
+  </header>
+
+  <nav>
+    <!-- Your navigation menu goes here -->
+  </nav>
+
+  <main>
+    <!-- Your main content goes here -->
+  </main>
+
+  <footer>
+    <!-- Your footer content goes here -->
+  </footer>
+</body>
+</html>
