a64c226546399798534b47e5c4a4ce3b8931b9bb
piojagcodes <piojag@gmail.com>
2023-05-23 09:11:38 +0200

    Form currently have implemented handling error while filling field ,, that will show error 1045(mistake in password and userr) and 1049(unknown database).System.Management.Automation.Internal.Host.InternalHost not implemented yet.

--- login.php
+++ login.php
@@ -1,16 +1,20 @@
 <?php
     require_once "connect.php"; // Include the "connect.php" file for establishing a database connection
 
-    $connection = @new mysqli($host, $db_user, $db_password, $db_name); // Create a new instance of the mysqli class for database connection
+    mysqli_report(MYSQLI_REPORT_ERROR | MYSQLI_REPORT_STRICT); // Set the error reporting mode
 
-    if ($connection->connect_errno != 0) { // Check if there is a connection error
-        echo "Error: " . $connection->connect_errno; // Output the connection error code
-    } else {
+
+    try{
+    $connection = @new mysqli($host, $db_user, $db_password, $db_name); // Create a new instance of the mysqli class for database connection
+    } catch (mysqli_sql_exception $exception) {
+        echo "Error: " . mysqli_connect_errno();
+        exit;
+    }
         $login = $_POST['login']; // Get the value of the 'login' input from the POST request
         $password = $_POST['password']; // Get the value of the 'password' input from the POST request
 
         echo "It works!"; // Output the message "It works!"
 
         $connection->close(); // Close the database connection
-    }
+    
 ?>
